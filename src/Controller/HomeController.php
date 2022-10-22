469f0b90c8f423260e4700a5f5bc0539919ec06a
abayasb <79287819+abayasb@users.noreply.github.com>
2022-10-21 22:40:16 -0500

    controladores

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="app_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'hello' => 'hola desde symfony'
+        ]);
+    }
+
+    public function animal($nombre): Response
+    {
+        $text = "Controlador de animales";
+        return $this->render(
+            'home/animal.html.twig',
+            [
+                'text' => $text,
+                'nombre' => $nombre
+            ]
+        );
+    }
+}
